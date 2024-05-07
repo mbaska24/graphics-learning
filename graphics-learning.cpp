061684e742f05470312b6395e61ddf9189f14329
ChildishDev <mertbaska00@gmail.com>
2024-05-07 19:36:11 +0200

    Add project files.

---
+++ graphics-learning.cpp
@@ -0,0 +1,12 @@
+﻿// graphics-learning.cpp : Defines the entry point for the application.
+//
+
+#include "graphics-learning.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
